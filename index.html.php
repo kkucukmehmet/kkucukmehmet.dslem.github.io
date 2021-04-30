ccb6c6a4cc85241ff1c009a0623a92f185bd02f5
Mehmet E KÜÇÜK <80420622+kkucukmehmet@users.noreply.github.com>
2021-04-30 03:26:57 +0300

    Add files via upload

---
+++ index.html.php
@@ -0,0 +1,28 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Başlıksız Belge</title>
+</head>
+
+<body>
+<?php
+$a=999;
+$b=12;
+$toplam=$a+$b;
+$fark=$a-$b;
+$carpim=$a*$b;
+$bolum=$a/$b;
+$kalan=$a%$b;
+echo "TOPLAM".$toplam;
+echo "<br>";
+echo "FARK".$fark;
+echo "<br>";
+echo "ÇARPIM".$carpim;
+echo "<br>";
+echo "BÖLÜM".$bolum;
+echo "<br>";
+echo "KALAN".$kalan;
+?>
+</body>
+</html>
